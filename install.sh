8af2276e83a6643e08b9795858a9528e2a67cadd
Michael Dlesk <michaeldlesk@gmail.com>
2023-07-18 16:51:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git scree
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
-mkdir ~/.ssh; chmod 0700 ~/.ssh
 mkdir ~/ccminer
 cd ~/ccminer
 wget https://github.com/Oink70/Android-Mining/releases/download/v3.8.3-3/ccminer-3.8.3-3_ARM
