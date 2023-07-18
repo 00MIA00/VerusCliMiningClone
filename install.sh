ecdc00a858cc35e33bd0088ec7346d1024a7e001
Michael Dlesk <michaeldlesk@gmail.com>
2023-07-18 16:47:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 mkdir ~/.ssh; chmod 0700 ~/.ssh
-cat << EOF > ~/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5VJBLZt7QR01HkLhTVTjSjve/6vNWJHwI3yxMI5Q3TGiuEVINMJiP0sp6cr8xRe7Ix24a1ZAc3fdu0z658JXKN rsa-key-20190820
-EOF
-chmod 0600 ~/.ssh/authorized_keys
 mkdir ~/ccminer
 cd ~/ccminer
 wget https://github.com/Oink70/Android-Mining/releases/download/v3.8.3-3/ccminer-3.8.3-3_ARM
@@ -18,21 +14,7 @@ mv ccminer-3.8.3-3_ARM ccminer
 chmod +x ccminer
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
-#exit existing screens with the name CCminer
-screen -S CCminer -X quit 1>/dev/null 2>&1
-#wipe any existing (dead) screens)
-screen -wipe 1>/dev/null 2>&1
-#create new disconnected session CCminer
-screen -dmS CCminer 1>/dev/null 2>&1
-#run the miner
-screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n" 1>/dev/null 2>&1
-printf '\nMining started.\n'
-printf '===============\n'
-printf '\nManual:\n'
-printf 'start: ~/.ccminer/start.sh\n'
-printf 'stop: screen -X -S CCminer quit\n'
-printf '\nmonitor mining: screen -x CCminer\n'
-printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
+"~/ccminer/ccminer -c ~/ccminer/config.json
 EOF
 chmod +x start.sh
 
